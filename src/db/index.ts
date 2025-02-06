5298217560891160a6b84db0ab8cef1301ed3eb2
Nick Lanzo <nlanzo@gmail.com>
2025-02-06 00:51:12 -0600

    update to use AWS-RDS postgres database rather than local database

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,22 @@
 import "dotenv/config"
+import fs from "fs"
+import path from "path"
 import { drizzle } from "drizzle-orm/node-postgres"
+import pkg from "pg"
 
-export const db = drizzle(process.env.DATABASE_URL!)
+const { Pool } = pkg
+
+const connectionString = process.env.DATABASE_URL!
+const caCertPath =
+  process.env.PG_SSL_CA ||
+  path.join(process.cwd(), "certs", "us-east-2-bundle.pem")
+
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(caCertPath),
+  },
+})
+console.log(caCertPath)
+export const db = drizzle(pool)
