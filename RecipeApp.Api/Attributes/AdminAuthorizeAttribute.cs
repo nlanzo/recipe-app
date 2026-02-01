ee40ae6336871ccf330c49cc4a6bf9d1c2c03a67
Nick Lanzo <nlanzo@gmail.com>
2026-02-01 15:08:36 -0600

    add missing using directive for Microsoft.Extensions.DependencyInjection in AdminAuthorizeAttribute.cs

--- RecipeApp.Api/Attributes/AdminAuthorizeAttribute.cs
+++ RecipeApp.Api/Attributes/AdminAuthorizeAttribute.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using RecipeApp.Api.Data;
 
 namespace RecipeApp.Api.Attributes;
